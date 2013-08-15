974f7cc6d1f720ca01f854453d9711aadb6bb843
crw-riviere <crw.riviere@gmail.com>
2013-08-15 21:58:18 +1000

    Implemented Database CRUD Framework
    Framework uses naming convention to perform simple CRUD operations on
    IndexedDB database.

--- viewmodel.js
+++ viewmodel.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
     var wo5 = {
         opReadWrite: 'readwrite',
+        dbName: 'WO5',
         tblProgramName: 'tblProgramVM',
-        tblDayName: 'tblDayVM'
+        tblDayName: 'tblDayVM',
+        tblExerciseName: 'tblExerciseVM',
+        tblSessionName: 'tblSessionVM',
+        tblSetName: 'tblSetVM'
     };
 
     var BreadCrumbVM = function (data) {
@@ -10,10 +14,16 @@ $(document).ready(function () {
         this.crumbs = ko.observableArray(data.crumbs);
     };
 
+    var BaseViewModel = function (data) {
+        this.id = data.id;
+        this.editable = ko.observable(data.editable);
+        this.name = data.name;
+    };
+
     var SetVM = function (data) {
         self = this;
         self.id = data.id;
-        self.sessionId = data.sessionId;
+        self.parentId = data.parentId;
         self.type = ko.observable('SetData');
         self.editable = ko.observable(data.editable);
         self.date = ko.observable(data.date);
@@ -21,83 +31,105 @@ $(document).ready(function () {
         self.weight = ko.observable();
     };
 
-    var SessionVM = function (data) {
-        self = this;
-        self.id = data.id;
-        self.exerciseId = data.exerciseId;
-        self.type = ko.observable('SessionData');
-        self.editable = ko.observable(data.editable);
-        self.name = ko.observable(data.name);
-        self.sets = ko.observableArray(data.sets);
-    };
-
-    var ExerciseVM = function (data) {
-        self = this;
-        self.id = data.id;
-        self.dayId = data.dayId;
-        self.type = ko.observable('ExerciseData');
-        self.editable = ko.observable(data.editable);
-        self.name = ko.observable(data.name);
-        self.sessions = ko.observableArray(data.sessions);
-    };
-
-    var DayVM = function (data) {
-        this.id = data.id;
-        this.programId = data.programId;
-        this.type = ko.observable('DayData');
-        this.editable = ko.observable(data.editable);
-        this.name = ko.observable(data.name);
-    };
-
-    var ProgramVM = function ProgramVM(data) {
-        var self = this;
-        self.id = data.id;
-        self.editable = ko.observable(data.editable);
-        self.name = ko.observable(data.name);
-    };
-
     var Workout5VM = function () {
         var self = this;
 
+        self.ProgramVM = function (data) {
+            this.id = data.id;
+            this.name = ko.observable(data.name);
+            this.type = 'ProgramVM';
+            this.childType = 'DayVM';
+            this.editable = ko.observable(data.editable);
+        };
+
+        self.DayVM = function (data) {
+            this.id = data.id;
+            this.programId = data.programId;
+            this.name = ko.observable(data.name);
+            this.type = 'DayVM';
+            this.childType = 'ExerciseVM';
+            this.editable = ko.observable(data.editable);
+        };
+
+        self.ExerciseVM = function (data) {
+            this.id = data.id;
+            this.dayId = data.dayId;
+            this.name = ko.observable(data.name);
+            this.type = 'ExerciseVM';
+            this.childType = 'SessionVM';
+            this.editable = ko.observable(data.editable);
+        };
+
+        self.SessionVM = function (data) {
+            this.id = data.id;
+            this.exerciseId = data.exerciseId;
+            this.name = ko.observable(data.name);
+            this.type = 'SessionVM';
+            this.childType = 'SetVM';
+            this.editable = ko.observable(data.editable);
+        };
+
+        self.SetVM = function (data) {
+            this.id = data.id;
+            this.parentId = data.parentId;
+            this.type = 'SetVM';
+            this.name = ko.observable(data.name);
+            this.reps = ko.observable(data.reps);
+            this.weight = ko.observable(data.weight);
+            this.editable = ko.observable(data.editable);
+        };
+
         self.listProgramVM = ko.observableArray();
         self.listDayVM = ko.observableArray();
+        self.listExerciseVM = ko.observableArray();
+        self.listSessionVM = ko.observableArray();
+        self.listSetVM = ko.observableArray();
         self.crumbs = ko.observableArray();
 
-        self.selectedWorkoutData = ko.observable(this);
-        self.selectedProgramData = ko.observable();
-        self.selectedDayData = ko.observable();
-        self.selectedSessionData = ko.observable();
-        self.selectedExerciseData = ko.observable();
-        self.selectedSetData = ko.observable();
+        self.selectedWorkoutVM = ko.observable(true);
+        self.selectedProgramVM = ko.observable(false);
+        self.selectedDayVM = ko.observable(false);
+        self.selectedSessionVM = ko.observable(false);
+        self.selectedExerciseVM = ko.observable(false);
+        self.selectedSetVM = ko.observable(false);
 
         self.InitDB = function () {
-            var request = window.indexedDB.open("WO5", 3);
+            var request = window.indexedDB.open(wo5.dbName, 1);
 
             request.onupgradeneeded = function () {
-                console.log('Initiating upgrade...');
+                console.log('Upgrading Database...');
                 var db = request.result;
+
                 var storeProgram = db.createObjectStore(wo5.tblProgramName, { keyPath: 'id', autoIncrement: true });
+                console.log('Created table: ' + wo5.tblProgramName);
                 var storeDay = db.createObjectStore(wo5.tblDayName, { keyPath: 'id', autoIncrement: true });
+                console.log('Created table: ' + wo5.tblDayName);
+                var storeExercise = db.createObjectStore(wo5.tblExerciseName, { keyPath: 'id', autoIncrement: true });
+                console.log('Created table: ' + wo5.tblExerciseName);
+                var storeExercise = db.createObjectStore(wo5.tblSessionName, { keyPath: 'id', autoIncrement: true });
+                console.log('Created table: ' + wo5.tblSessionName);
+                var storeExercise = db.createObjectStore(wo5.tblSetName, { keyPath: 'id', autoIncrement: true });
+                console.log('Created table: ' + wo5.tblSetName);
             };
 
-            request.onerror = function (event) {
-                console.log('Error: ' + event);
+            request.onerror = function (ex) {
+                console.log('Open Error: ' + ex.message);
             };
 
             request.onsuccess = function (event) {
-                console.log('Success: ' + event);
+                console.log('Opened Database: ' + event.target.result);
                 wo5.db = request.result;
 
-                self.loadTable(ProgramVM);
+                self.loadTable('ProgramVM');
             };
         };
 
         self.loadTable = function (viewModel) {
-            var constructorName = new viewModel({}).constructor.name;
-            var tblName = 'tbl'.concat(constructorName);
+            var tblName = 'tbl'.concat(viewModel);
             console.log('Loading table ' + tblName);
 
-            var listViewModel = 'list'.concat(constructorName);
+            var listViewModel = 'list'.concat(viewModel);
+            console.log('list: ' + listViewModel);
 
             var tbl = wo5.db.transaction(tblName, wo5.opReadWrite).objectStore(tblName);
 
@@ -107,30 +139,87 @@ $(document).ready(function () {
                 var cursor = event.target.result;
 
                 if (cursor) {
-                    self[listViewModel].push(new viewModel(cursor.value));
+                    self[listViewModel].push(new self[viewModel](cursor.value));
                     console.log('Added ' + cursor.value.name + ' to ' + listViewModel);
 
                     cursor.continue();
                 }
             };
+
+            csr.onerror = function (event) {
+                console.log('Cursor Error: ' + event);
+            };
         };
 
-        self.gotoExercise = function (exercise) {
-            self.selectedExerciseData(exercise);
-            self.selectedDayData(null);
-            self.addBreadCrumb(exercise);
+        self.addEntity = function (viewModel) {
+            var tblName = 'tbl'.concat(viewModel);
+            var listName = 'list'.concat(viewModel);
+            var tblViewModel = wo5.db.transaction(tblName, wo5.opReadWrite).objectStore(tblName);
+
+            var addResult = tblViewModel.add({});
+
+            addResult.onsuccess = function (event) {
+                self[listName].push(new self[viewModel]({ id: event.target.result, name: '', editable: true }));
+            };
         };
 
-        self.gotoSession = function (session) {
-            self.selectedSessionData(session);
-            self.selectedExerciseData(null);
-            self.addBreadCrumb(session);
+        self.saveEntity = function (viewModel, entity) {
+            entity.editable(false);
+
+            var tblName = 'tbl'.concat(viewModel);
+            var listName = 'list'.concat(viewModel);
+
+            var tblViewModel = wo5.db.transaction(tblName, wo5.opReadWrite).objectStore(tblName);
+            var saveRequest = tblViewModel.put(ko.toJS(entity));
+
+            saveRequest.onsuccess = function (event) {
+                console.log('Updated entity: ' + event.target.result);
+            };
+
+            saveRequest.onerror = function (event) {
+                console.log('Error updating entity: ' + event.message);
+            };
         };
 
-        self.gotoSet = function (set) {
-            self.selectedSetData(set);
-            self.selectedSessionData(null);
-            self.addBreadCrumb(set);
+        self.deleteEntity = function (viewModel, entity) {
+            var tblName = 'tbl'.concat(viewModel);
+            var listName = 'list'.concat(viewModel);
+
+            var tblViewModel = wo5.db.transaction(tblName, wo5.opReadWrite).objectStore(tblName);
+            var delRequest = tblViewModel.delete(entity.id);
+
+            delRequest.onsuccess = function (event) {
+                console.log('Removed entity: ' + event.target.result);
+                self[listName].remove(entity);
+            };
+
+            delRequest.onerror = function (event) {
+                console.log('Error updating entity: ' + event.message);
+            };
+        };
+
+        self.gotoEntity = function (viewModel, entity) {
+            var isParent = false;
+            var childVM;
+            for (var prop in self) {
+                if (~prop.indexOf('selected')) {
+                    self[prop](false);
+                }
+
+                if (isParent === true) {
+                    childVM = prop.toString();
+
+                    isParent = false;
+                };
+
+                if (prop.toString() === viewModel) {
+                    isParent = true;
+                }
+            }
+
+            var selectedVM = 'selected'.concat(viewModel);
+            self[selectedVM](true);
+            self.loadTable(childVM);
         };
 
         self.gotoBreadCrumb = function (crumb) {
@@ -160,90 +249,6 @@ $(document).ready(function () {
                     self[prop](null);
                 }
             }
-        };
-
-        self.addProgram = function () {
-            var tblProgram = wo5.db.transaction(wo5.tblProgramName, wo5.opReadWrite).objectStore(wo5.tblProgramName);
-            var addResult = tblProgram.add({});
-
-            addResult.onsuccess = function (event) {
-                self.programs.push(new ProgramVM({ id: event.target.result, name: '', editable: true }));
-            };
-        };
-
-        self.deleteProgram = function (program) {
-            var tblProgram = wo5.db.transaction(wo5.tblProgramName, wo5.opReadWrite).objectStore(wo5.tblProgramName);
-
-            console.log('Program Id: ' + program.id);
-            var delRequest = tblProgram.delete(program.id);
-
-            delRequest.onsuccess = function (event) {
-                console.log('Removed program: ' + event.target.result);
-                self.programs.remove(program);
-            };
-
-            delRequest.onerror = function (event) {
-                console.log('Error removing program: ' + event.message);
-            };
-        };
-
-        self.saveProgram = function (program) {
-            program.editable(false);
-            var tblProgram = wo5.db.transaction(wo5.tblProgramName, wo5.opReadWrite).objectStore(wo5.tblProgramName);
-
-            var saveRequest = tblProgram.put(ko.toJS(program));
-
-            saveRequest.onsuccess = function (event) {
-                console.log('Updated program: ' + event.target.result);
-            };
-
-            saveRequest.onerror = function (event) {
-                console.log('Error updating program: ' + event.message);
-            };
-        };
-
-        self.gotoProgram = function (program) {
-            self.selectedProgramData(program);
-            self.selectedWorkoutData(null);
-            self.addBreadCrumb(program);
-            self.loadDays();
-        };
-
-        self.gotoDay = function (day) {
-            self.selectedDayData(day);
-            self.selectedProgramData(null);
-            self.addBreadCrumb(day);
-        };
-
-        self.addSession = function (session) {
-            self.selectedExerciseData().sessions
-
-                .push(new SessionVM({ name: '', sets: [], editable: true }));
-        };
-
-        self.saveSession = function (session) {
-            console.log(session);
-            session.editable(false);
-        };
-
-        self.deleteSession = function (session) {
-            self.selectedExerciseData().sessions
-            .remove(session);
-        };
-
-        self.addSet = function (set) {
-            self.selectedSessionData().sets
-            .push(new SetVM({ name: '', reps: 0, weight: 0, editable: true }));
-        };
-
-        self.saveSet = function (set) {
-            console.log();
-            localStorage.proj = ko.toJSON(self.programs);
-        };
-
-        self.deleteSet = function (set) {
-            self.selectedSessionData().sets
-            .remove(set);
         };
 
         self.InitDB();

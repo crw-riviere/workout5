bd63249fcd184de8b06d891895662e0e8b6880e8
crw-riviere <crw.riviere@gmail.com>
2013-09-03 23:27:19 +1000

    Added Loading Screen
    A load screen is displayed upon initialization of the app and fades out
    once all resources have loaded.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(window).load(function () {
+    $('#homeloadingPanel').fadeOut(2000, function () {
+        $('#loadingBall').removeClass('play-loading-ball');
+        //$('#homeContainer').addClass('hidden');
+        //$('#contentContainer').removeClass('hidden');
+    });
+});

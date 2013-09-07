ccc8989487db37abd17eb781b15e4c680a1a189c
crw-riviere <crw.riviere@gmail.com>
2013-09-07 19:44:05 +1000

    UI Change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
-$(window).load(function () {
-    $('#homeloadingPanel').fadeOut(2000, function () {
-        $('#loadingBall').removeClass('play-loading-ball');
-        //$('#homeContainer').addClass('hidden');
-        //$('#contentContainer').removeClass('hidden');
+$(document).ready(function () {
+    $(window).load(function () {
+        $('#pnlHomeLoad').fadeOut(1000, function () {
+            $('#pnlLoadingBall').removeClass('animation-loading-ball');
+            $('#pnlHomeNav').fadeIn('slow', function () {
+                // Animation complete
+            });
+        });
+    });
+
+    $('.btnWorkout').click(function () {
+        $('#pgeSettings').addClass('hidden');
+        $('#pgeHome').addClass('hidden');
+        $('#pgeWorkout').removeClass('hidden');
+    });
+
+    $('#btnSettings').click(function () {
+        $('#pgeHome').addClass('hidden');
+        $('#pgeSettings').removeClass('hidden');
+    });
+
+    $('.btnHome').click(function () {
+        $('#pgeSettings').addClass('hidden');
+        $('#pgeHome').removeClass('hidden');
     });
 });

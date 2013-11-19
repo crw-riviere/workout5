f3b4c43f6300742c70d0b00f7d0c4b3e3d90e5ad
Charly Riviere <crw.riviere@gmail.com>
2013-11-19 21:19:10 +1100

    Firefox OS Install fixes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
-$(document).ready(function () {
-    $(window).load(function () {
-        $('#pnlHomeLoad').fadeOut(1000, function () {
-            $('#pnlLoadingBall').removeClass('animation-loading-ball');
-            $('#pnlHomeNav').fadeIn('slow', function () {
-                // Animation complete
-            });
-        });
-    });
-
-    $('.btnWorkout').click(function () {
-        $('#pgeSettings').addClass('hidden');
-        $('#pgeHome').addClass('hidden');
-        $('#pgeWorkout').removeClass('hidden');
-    });
-
-    $('#btnSettings').click(function () {
-        $('#pgeHome').addClass('hidden');
-        $('#pgeSettings').removeClass('hidden');
-    });
-
-    $('.btnHome').click(function () {
-        $('#pgeSettings').addClass('hidden');
-        $('#pgeHome').removeClass('hidden');
-    });
-});
+var checkRequest = navigator.mozApps.checkInstalled("http://wo5.riviere.la/wo5.webapp");
+checkRequest.onsuccess = function () {
+    if (checkRequest.result) {
+        // we're installed
+    } else {
+        // not installed
+        var installRequest = navigator.mozApps.install("http://wo5.riviere.la/wo5.webapp");
+        request.onsuccess = function () {
+            window.alert('Installed Workout5.');
+        };
+        request.onerror = function () {
+            window.alert('Failed to install Workout5.');
+        };
+    }
+};
+checkRequest.onerror = function () {
+    alert('Error checking installation status: ' + this.error.message);
+};
